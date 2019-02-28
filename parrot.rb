f6a77eddbf39a5b52f886484f9feefa0038c995e
Adam Walker <adamw207@yahoo.com>
2019-02-28 19:07:49 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,2 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot name = "Squawk!" puts name return name end# Create method `parrot` that outputs a given phrase and
 # returns the phrase
